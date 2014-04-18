af73c0326c7276407f86328b58d336cfcfb60e16
Phil Ananin <phil.ananin@gmail.com>
2014-04-18 22:58:30 +0100

    parse band list (just print out names for now)

--- scraper.js
+++ scraper.js
@@ -2,14 +2,28 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var request = require('request');
 
-var url = 'http://metalstorm.net/bands/band.php?band_id=13&bandname=Opeth';
+var url = 'http://metalstorm.net/bands';
+
+function parseBands(rawHtml) {
+  var $ = cheerio.load(rawHtml);
+  var content = $('#td_content');
+  var bandsTable = content.children().first().find('table.bordercolor2');
+  var bands = bandsTable.children();
+  if(!bands || bands.length === 0) {
+    console.log('no bands found');
+  }
+  bands.each(function(idx, element) {
+    if(idx === 0)
+      return;
+    var band = $(this);
+    console.log(band.children().eq(1).text().trim());
+  });
+}
 
 request(url, function(err, res, html) {
   if(!err) {
-    var $ = cheerio.load(html);
-    var bandname = $('.page_title').text().trim();
-    console.log('band: ' + bandname);
+    parseBands(html);
   } else {
-    console.log('error');
+    console.log('error: ' + err);
   }
 });
