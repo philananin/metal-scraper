2dda5979bf8cce65c96d21eca3d090ef4b8701fa
Phil Ananin <phil.ananin@gmail.com>
2014-04-19 22:59:25 +0100

    add scrape task

--- gruntfile.js
+++ gruntfile.js
@@ -2,10 +2,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['*.js']
+    },
+    nodemon: {
+      dev: {
+        script: [ 'scraper.js' ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-nodemon');
 
   grunt.registerTask('default', [ 'jshint' ]);
+  grunt.registerTask('scrape', 'nodemon');
 };
