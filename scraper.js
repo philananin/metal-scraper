11e66e5adbe77ca049f4138c5b89e424de32705b
Phil Ananin <phil.ananin@gmail.com>
2014-04-20 22:26:15 +0100

    tidyup

--- scraper.js
+++ scraper.js
@@ -5,35 +5,50 @@ var request = require('request');
 function parseBands(rawHtml, pageNumber) {
   var $ = cheerio.load(rawHtml);
   var content = $('#td_content');
+
   var bandsTable = content.children().first().find('table.bordercolor2');
   var bands = bandsTable.children();
   if(!bands || bands.length === 0) {
     console.log('no bands found');
   }
-  bands.each(function(idx, element) {
-    if(idx === 0)
+
+  bands.each(function(index) {
+    if(index === 0)
       return;
-    var band = $(this);
-    var name = band.children().eq(1).text().trim();
-    var url = band.children().eq(1).find('a').attr('href');
+    var band = $(this).children().eq(1); // second child td
+    var name = band.text().trim();
+    var url = band.find('a').attr('href');
     var bandId = url.split('=')[1];
     console.log('band: ' + name + ', id: ' + bandId);
   });
 
-  var nextPages = content.find('table').filter(function filter(index, element) {
-    var table = $(this);
-    return table.text().indexOf('Results:') > -1;
-  }).find('a').filter(function(idx, elm) {
-    var a = $(this);
-    var linkPageNumber = parseInt(a.text());
-    return linkPageNumber > pageNumber;
-  });
+  function getNextPage(content, currentPageNumber) {
+    function isResultsTable(index, element) {
+      var table = $(this);
+      return table.text().indexOf('Results:') > -1;
+    }
 
-  if (nextPages.length > 0) {
-    var nextPage = nextPages.first();
+    function pageNumberGreaterThanCurrent(idx, elm) {
+      var a = $(this);
+      var linkPageNumber = parseInt(a.text());
+      return linkPageNumber > currentPageNumber;
+    }
+
+    var nextPages = content
+    .find('table')
+    .filter(isResultsTable)
+    .find('a')
+    .filter(pageNumberGreaterThanCurrent);
+
+    if (nextPages.length > 0) {
+      return nextPages.first();
+    }
+  }
+
+  var nextPage = getNextPage(content, pageNumber);
+  if(nextPage) {
     var url = 'http://metalstorm.net/bands/' + nextPage.attr('href');
     var number = parseInt(nextPage.text());
-    console.log(number + ', ' + url);
     setTimeout(downloadBandPage(url, number), 100);
   }
 }
