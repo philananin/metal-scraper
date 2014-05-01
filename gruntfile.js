326669fc6f96c21cb6471caa46ab017a7bcbbdab
Phil Ananin <phil.ananin@gmail.com>
2014-05-01 22:49:28 +0100

    add mocha to grunt/npm properly

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,34 @@
 module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true
+        },
+        src: ['test/*.js']
+      },
+    },
+
+    watch: {
+      scripts: {
+        options: {
+          spawn: false,
+        },
+        files: ['**/*.js', '!**/node_modules/**'],
+        tasks: ['jshint', 'mochaTest']
+      }
+    },
+
     jshint: {
       all: ['*.js', 'utils/*.js', 'processors/*.js', 'test/*.js']
-    },
-    nodemon: {
-      dev: {
-        script: [ 'scraper.js' ]
-      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-nodemon');
-
-  grunt.registerTask('default', [ 'jshint' ]);
-  grunt.registerTask('scrape', 'nodemon');
+  grunt.registerTask('default', [ 'jshint', 'mochaTest' ]);
 };
