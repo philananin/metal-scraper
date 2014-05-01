8c8d72dbbf0caa9aefe897f431603fd920f27fa5
Phil Ananin <phil.ananin@gmail.com>
2014-05-01 23:19:04 +0100

    add genres test

--- test/bandProcessorSpec.js
+++ test/bandProcessorSpec.js
@@ -12,9 +12,38 @@ describe('BandProcessor', function() {
   });
 
   describe('process', function() {
-    it('returns the correct band name', function(done) {
+    it('extracts the correct band name and id', function(done) {
       bandProcessor.on('parse', function(bandDetails) {
         expect(bandDetails.name).to.equal('Periphery');
+        expect(bandDetails.id).to.equal('5269');
+        done();
+      });
+      bandProcessor.process(html);
+    });
+
+    it('extracts the correct genres', function(done) {
+      var expectedGenres = [{
+        fullName: 'Progressive metal',
+        main: 'Progressive',
+        prefix: '',
+        startYear: '2005',
+        endYear: ''
+      }, {
+        fullName: 'Math metal',
+        main: 'Math',
+        prefix: '',
+        startYear: '2005',
+        endYear: ''
+      }, {
+        fullName: 'Djent',
+        main: '',
+        prefix: 'Djent',
+        startYear: '2005',
+        endYear: ''
+      }];
+
+      bandProcessor.on('parse', function(bandDetails) {
+        expect(bandDetails.genres).to.deep.equal(expectedGenres);
         done();
       });
       bandProcessor.process(html);
